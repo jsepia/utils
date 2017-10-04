c2207f8b6cb23ad4a46c9830bf0f5821865de815
sepiaj <sepiaj@amazon.com>
2017-10-04 13:43:08 -0700

    Fix integ test

--- test/integ/index.js
+++ test/integ/index.js
@@ -2,7 +2,7 @@ const nodeVersion = require('node-version')
 
 if (nodeVersion) {
   const majorVersion = parseInt(nodeVersion.major)
-  if (majorVersion > 7) {
+  if (majorVersion > 8) {
     require('./import')
   }
   else {
